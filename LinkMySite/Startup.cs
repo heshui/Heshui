cf573a653253b538e2cec6af8d128a7220fc0c75
ccxiaohui521 <ccxiaohui521@163.com>
2018-06-13 11:06:46 +0800

    添加项目文件。

---
+++ LinkMySite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LinkMySite.Startup))]
+namespace LinkMySite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
